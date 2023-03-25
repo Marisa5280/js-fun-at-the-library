bfba7d297774af2d87e0c18f8910efa9580af8f1
Marisa Wyatt <risawyatt22@gmail.com>
2023-03-25 15:27:31 -0600

    Add funtions to pass shelf tests

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,46 @@
 
+function shelfBook(book, shelf) {
+  if(shelf.length < 3) {
+  shelf.unshift(book);
+  console.log(shelf)
+  return shelf
+  }
+};
+// function unshelfBook(book, shelf) {
+//   var indexPos = shelf.indexOf(book);
+//    shelf.splice(indexPos);
+//    return shelf
+//  };
+ 
+function unshelfBook(book, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === book)
+    shelf.splice(i, 1)
+  }
+  return shelf
+};
+
+function listTitles (shelf) {
+ var titles = [];
+  for (var i = 0; i < shelf.length; i++) {
+  titles.push(shelf[i].title);
+ }
+ return titles.join(", ")
+};
+
+function searchShelf(shelf, title) {
+  for (var i = 0; i < shelf.length; i++) {
+    var shelved = false
+    if(shelf[i].title === title) {
+    shelved = true
+    }
+  }
+  return shelved
+}
+
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
